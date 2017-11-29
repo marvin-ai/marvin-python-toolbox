9f9afbc21b2c8638e785c9ce24fd21189e32c7f6
lucasbm88 <lucasbonatto@gmail.com>
2017-11-29 17:16:38 -0200

    Removing unneeded files.

--- marvin_python_toolbox/management/templates/python-engine/docker/marvin-base-docker/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -z "$1" ]
-  then
-    echo "You must specify the version of the image being built"
-    exit 1
-fi
-docker build -t registry.b2w.io/b2wdigital/predictionio-b2w:"$1" .
-
-
