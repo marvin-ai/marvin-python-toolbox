fb18ddc4ed8e48eeee6f0d35269442d68c6f6e0e
zhang.yifei <zhang.yifei@b2wdigital.com>
2018-06-19 17:34:23 -0300

    unit test fix

--- tests/engine_base/test_engine_base_action.py
+++ tests/engine_base/test_engine_base_action.py
@@ -15,7 +15,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import unicode_literals
 import joblib as serializer
 import pytest
 import os
@@ -112,7 +111,7 @@ class TestEngineBaseAction:
             assert False
 
         except Exception as e:
-            assert str(e) == "('MultipleAssignException', '_params')"
+            assert unicode(e) == "('MultipleAssignException', '_params')"
 
     def test_load_obj_local_persistence(self, engine_action):
         engine_action2 = copy.copy(engine_action)
